38bb944b8dc2f07ab2b33e7bdc770209d9a9a783
Luan Braz <luan_silvacpv@hotmail.com>
2021-11-22 15:12:25 -0300

    criado atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,18 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=35):
+        self.idade = idade
+        self.nome = None
+
     def cumprimentar(self):
-        return 'Olá'
+        return f'Olá{id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Luciano')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Luan'
+    print(p.nome)
+    print(p.idade)
