d4d67911412db926c408bd55c1e5ba63eb4ca3dd
Luan Braz <luan_silvacpv@hotmail.com>
2021-11-23 16:47:19 -0300

    criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,20 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):                            # *filhos->> qtd variavel de filhos
         self.idade = idade
-        self.nome = None
+        self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá{id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Luciano')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Luan'
-    print(p.nome)
-    print(p.idade)
+    luan = Pessoa(nome ='Luan')
+    edson = Pessoa(luan, nome ='Edson')
+    print(Pessoa.cumprimentar(edson))
+    print(id(edson))
+    print(edson.cumprimentar())
+    print(edson.nome)
+    print(edson.idade)
+    for filho in edson.filhos:
+        print(filho.nome)
