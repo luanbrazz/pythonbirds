8d9d1c59488fd1f79f6de595cc52af27de54d6d6
Luan Braz <luan_silvacpv@hotmail.com>
2021-11-24 18:33:14 -0300

    Implementação do Carro.

--- oo/carro.py
+++ oo/carro.py
@@ -44,28 +44,28 @@ O       L
     >>> direcao.valor
     'Norte'
     >>> direcao.girar_a_direita()
-    >>> direcao_valor
+    >>> direcao.valor
     'Leste'
     >>> direcao.girar_a_direita()
-    >>> direcao_valor
+    >>> direcao.valor
     'Sul'
     >>> direcao.girar_a_direita()
-    >>> direcao_valor
+    >>> direcao.valor
     'Oeste'
     >>> direcao.girar_a_direita()
-    >>> direcao_valor
+    >>> direcao.valor
     'Norte'
     >>> direcao.girar_a_esquerda()
-    >>> direcao_valor
+    >>> direcao.valor
     'Oeste'
     >>> direcao.girar_a_esquerda()
-    >>> direcao_valor
+    >>> direcao.valor
     'Sul'
     >>> direcao.girar_a_esquerda()
-    >>> direcao_valor
+    >>> direcao.valor
     'Leste'
     >>> direcao.girar_a_esquerda()
-    >>> direcao_valor
+    >>> direcao.valor
     'Norte'
     >>> carro = Carro(direcao, motor)
     >>> carro.calcular_velocidade()
@@ -91,6 +91,30 @@ O       L
     >>> carro.calcular_direção()
     'Oeste'
 """
+
+class Carro:
+    def __init__(self, direcao, motor):
+        self.motor = motor
+        self.direcao = direcao
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        self.motor.acelerar()
+
+    def frear(self):
+        self.motor.frear()
+
+    def calcular_direcao(self):
+        return self.direcao.valor()
+
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        self.direcao.girar_a_esquerda()
+
 
 NORTE='Norte'
 SUL='Sul'
